e1338c529897dc92b8e674f58d04fd9423bbf4ca
Dennis Duffy <dennis.duffy@state.mn.us>
2024-04-10 15:15:29 -0500

    wrote query to create production table

---
+++ Scripts/SQL/qryCreateMimProduction.sql
@@ -0,0 +1,15 @@
+CREATE TABLE mim_production(
+	id TEXT PRIMARY KEY,
+	address_county TEXT,
+	nameplate_kw REAL,
+	production_14 REAL, 
+	production_15 REAL, 
+	production_16 REAL, 
+	production_17 REAL, 
+	production_18 REAL, 
+	production_19 REAL, 
+	production_20 REAL, 
+	production_21 REAL, 
+	production_22 REAL, 
+	production_23 REAL
+)
